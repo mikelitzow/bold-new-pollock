032f2712d67b00df3c33a2b4208e3b938dacedd4
Mike Litzow <mlitzow@alaska.edu>
2020-02-28 15:47:19 -0900

    Cleaned up trawl processing

--- data/trawl data processing.R
+++ data/trawl data processing.R
@@ -40,31 +40,41 @@ for(i in 1:length(numeric)){
 
 # this is creating NAs, need to check and make sure this is OK!
 
-# now add NBS
-temp <- read.csv("data/cpue_nbs_FULL_allspecies_2010_17_18.csv")
+# try removing the rows w/ YEAR is na
+drop <- is.na(trawl.data$YEAR)
+which(drop==T)
 
-trawl.data <- rbind(trawl.data, temp)
+trawl.data[drop,] # yep, those are all junk!
+
+trawl.data <- trawl.data[!drop,]
+
+# now add NBS
+temp1 <- read.csv("data/cpue_nbs_FULL_allspecies_2010_2017.csv")
+temp2 <- read.csv("data/cpue_nbs_trunc_allspecies_2018.csv")
+temp3 <- read.csv(("data/nbs_2019_preliminary.csv"))
+trawl.data <- rbind(trawl.data, temp1, temp2, temp3)
+
 
 lat <- tapply(trawl.data$LATITUDE, list(trawl.data$YEAR, trawl.data$STATION), mean, na.rm=T)
 long <- tapply(trawl.data$LONGITUDE, list(trawl.data$YEAR, trawl.data$STATION), mean, na.rm=T)
 j <- apply(lat, 2, function(x) sum(is.na(x)))
 table(j)  
 
-ff <- function(x) sum(!is.na(x))
-
-sample.size <- apply(lat, 2, ff) # see that 'station' is a station name! I think are some colnames that got introduced into the data - need to check!
-
-# remove stations with no samples!
-keep <- sample.size > 0
-sample.size <- sample.size[keep]
+# ff <- function(x) sum(!is.na(x))
+# 
+# sample.size <- apply(lat, 2, ff) # see that 'station' is a station name! I think are some colnames that got introduced into the data - need to check!
+# 
+# # remove stations with no samples!
+# keep <- sample.size > 0
+# sample.size <- sample.size[keep]
 
 # get mean lat/long and plot
 mean.lat <- apply(lat, 2, mean, na.rm=T)
 mean.long <- apply(long, 2, mean, na.rm=T)
 
-# remove the bogus 'station'
-mean.lat <- mean.lat[keep]
-mean.long <- mean.long[keep]
+# # remove the bogus 'station'
+# mean.lat <- mean.lat[keep]
+# mean.long <- mean.long[keep]
 
 # and plot 
 
@@ -77,6 +87,16 @@ map('world2Hires', c('usa', 'USSR'),
 trawl.data <- trawl.data %>%
   filter(SID==21740)
 
+sum(trawl.data$WTCPUE==0) # only 12 catches==0!
+# so need to process and distinguish 0 catches from did not sample!
+# NB!!
+
+# for now (!!) 
+# I'll drop 0 catches
+# so the following is only for positive catches
+drop <- trawl.data$WTCPUE==0
+trawl.data <- trawl.data[!drop,]
+
 # fit some spatial GAMs to cpue by era
 library(mgcv)
 
@@ -87,24 +107,23 @@ mean.long <- tapply(trawl.data$LONGITUDE, trawl.data$STATION, mean)
 trawl.data$mean.lat <- mean.lat[match(trawl.data$STATION, names(mean.lat))]
 trawl.data$mean.long <- mean.long[match(trawl.data$STATION, names(mean.long))]
 
-
 fit.1 <- gam(log(WTCPUE) ~ s(mean.lat, mean.long, k=60), data=filter(trawl.data, YEAR <= 2013))
 fit.2 <- gam(log(WTCPUE) ~ s(mean.lat, mean.long, k=60), data=filter(trawl.data, YEAR > 2013))
 
 dir.create("figs", showWarnings = FALSE)
 
-png("figs/cpue by decade.png", 8, 3, units="in", res=300)
+png("figs/cpue by decade.png", 7, 3, units="in", res=300)
 
-par(mfrow=c(1,2), mar=c(2,2,1,1))
+par(mfrow=c(1,2), mar=c(2,2,1,1), las=1)
 zlim <- range(fitted(fit.1), fitted(fit.2))
 
-# 80s
+# 1982-2013
 vis.gam(fit.1, view = c("mean.long", "mean.lat"), n.grid=100, zlim=zlim,	plot.type="contour", color="topo", nlevels=8, labcex=0.8,
         too.far = 0.04, xlab = "", ylab = "", 	xlim=c(-180,-157), ylim=c(54,67), main="")  
 map('worldHires',fill=T,add=T)
 text(-160, 61, "1982-2013", col="white")
 
-# 90s
+# 2014-2019
 vis.gam(fit.2, view = c("mean.long", "mean.lat"), n.grid=100, zlim=zlim,	plot.type="contour", color="topo", nlevels=8, labcex=0.8,
               too.far = 0.04, xlab = "", ylab = "", 	xlim=c(-180,-157), ylim=c(54,67), main="")  
 map('worldHires',fill=T,add=T)
