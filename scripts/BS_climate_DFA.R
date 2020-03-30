0b9e283c1f74b00d939c498b496a60e921e283c1
KristaOke <kristaoke@gmail.com>
2020-03-30 12:41:37 -0800

    created new script to run a DFA on BS climate

---
+++ scripts/BS_climate_DFA.R
@@ -0,0 +1,131 @@
+#============================================================================================================================================
+# DFA on EBS climate
+
+#Created by Krista, March 2020
+#Borrowing heavily from 'example script -DFA on GOA climate and community data.R'
+#============================================================================================================================================
+#Notes:
+#============================================================================================================================================
+
+library(ggplot2)
+library(reshape2)
+library(dplyr)
+library(pracma)
+library(MARSS)
+library(tidyr)
+# devtools::install_github("kassambara/ggpubr")
+library(ggpubr)
+library(cowplot)
+library(gridExtra)
+library(broom)
+library(lemon)
+library(MuMIn)
+library(lmtest)
+library(cowplot)
+
+#Get data plot data =======
+
+# load environmental data
+dat <- read.csv("data/climate data.csv", row.names = 1)
+
+
+# plot TS for SI
+do.dat <- as.data.frame(scale(dat)) # scale to plot on 1 axis
+plot.dat <- gather(do.dat)
+plot.dat$year <- 1951:2019
+
+
+ggplot(plot.dat, aes(x=year, y=value)) + geom_bar(position="dodge", stat="identity", fill=cb[2]) + facet_wrap(~key) +
+  ylab("Standard anomaly") + xlab("") + theme_bw() + geom_hline(yintercept = 0)
+
+# check
+head(dat)
+
+#manupulate data for DFA========
+
+# save the full data set for later use...
+all.clim.dat <- t(as.matrix(dat))
+
+# now fit DFA models
+# first the early era
+# e.cli.dat = as.matrix(dat[rownames(dat) %in% 1950:1988,])
+# 
+# # and transpose
+# e.cli.dat <- t(e.cli.dat)
+
+
+#fit model========
+
+# now fit DFA models with 1-3 trends and different error structures and compare
+
+# changing convergence criterion to ensure convergence
+cntl.list = list(minit=200, maxit=20000, allow.degen=FALSE, conv.test.slope.tol=0.1, abstol=0.0001)
+
+# set up forms of R matrices
+levels.R = c("diagonal and equal",
+             "diagonal and unequal",
+             "equalvarcov",
+             "unconstrained")
+model.data = data.frame()
+
+# fit models & store results
+for(R in levels.R) {
+  for(m in 1:3) {  # allowing up to 3 trends
+    dfa.model = list(A="zero", R=R, m=m)
+    kemz = MARSS(all.clim.dat, model=dfa.model, control=cntl.list,
+                 form="dfa", z.score=TRUE)
+    model.data = rbind(model.data,
+                       data.frame(R=R,
+                                  m=m,
+                                  logLik=kemz$logLik,
+                                  K=kemz$num.params,
+                                  AICc=kemz$AICc,
+                                  stringsAsFactors=FALSE))
+    assign(paste("kemz", m, R, sep="."), kemz)
+  } # end m loop
+} # end R loop
+
+# calculate delta-AICc scores, sort in descending order, and compare
+model.data$dAICc <- model.data$AICc-min(model.data$AICc)
+model.data <- model.data %>%
+  arrange(dAICc)
+model.data
+
+#rotate=======
+
+# now fit best model
+
+model.list = list(A="zero", m=3, R="diagonal and unequal") # best model
+mod.best = MARSS(all.clim.dat, model=model.list, z.score=TRUE, form="dfa", control=cntl.list)
+
+
+# and rotate the loadings
+Z.est = coef(mod.best, type="matrix")$Z
+H.inv = varimax(coef(mod.best, type="matrix")$Z)$rotmat
+Z.rot = as.data.frame(Z.est %*% H.inv)
+
+# reverse trend 2 to plot
+Z.rot[,2] <- -Z.rot[,2]
+
+Z.rot$names <- rownames(all.clim.dat)
+Z.rot <- arrange(Z.rot, V1)
+Z.rot <- gather(Z.rot[,c(1,2)])
+Z.rot$names <- rownames(all.clim.dat)
+#Z.rot$plot.names <- reorder(Z.rot$names, 1:14)
+
+
+#plot=========
+# get CI and plot loadings...
+modCI <- MARSSparamCIs(mod.best)
+
+#BELOW HERE is where things get bad
+# plot.CI <- data.frame(names=rownames(all.clim.dat), mean=modCI$par$Z, upCI=modCI$par.upCI$Z,
+#                            lowCI=modCI$par.lowCI$Z)
+plot.CI <- data.frame(mean=modCI$par$Z, upCI=modCI$par.upCI$Z,
+                      lowCI=modCI$par.lowCI$Z)
+
+plot.CI <- arrange(plot.CI, mean)
+#plot.CI$names.order <- reorder(plot.CI$names, plot.CI$mean)
+dodge <- position_dodge(width=0.9)
+
+
