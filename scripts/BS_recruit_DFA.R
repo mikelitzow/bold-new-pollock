739b604f6c79b61402c382531b7c71f53ef40c9f
KristaOke <kristaoke@gmail.com>
2020-04-22 15:37:54 -0800

    log transformed and re-plotted

--- scripts/BS_recruit_DFA.R
+++ scripts/BS_recruit_DFA.R
@@ -46,11 +46,29 @@ ggplot(raw.rec.dat, aes(x=year, y=recruitment)) + geom_point() + geom_smooth() +
   facet_wrap(~species, scales="free") +
   ylab("Recruitment") + xlab("") + theme_bw() 
 
+#log transform====
+
+log_rec <- as.data.frame(cbind(rec_dat$year, log(rec_dat$YLFN.R.age1.18.1a), log(rec_dat$TRBT.Age.0.R.16.1b),
+                 log(rec_dat$FHSL.R.age0), log(rec_dat$Opilio.age0.recruits),
+                 log(rec_dat$cod.age0.R), log(rec_dat$pollock.age0.R)))
+#set col names
+log_rec <- log_rec %>% 
+  rename(
+    year = V1,
+    YLFN.R.age1.18.1a = V2,
+    TRBT.Age.0.R.16.1b = V3,
+    FHSL.R.age0 = V4,
+    Opilio.age0.recruits = V5,
+    cod.age0.R = V6,
+    pollock.age0.R = V7
+  )
 
 
+log.rec.dat <- gather(log_rec, key=species, value=log_recruitment, -year)
 
-
-
+ggplot(log.rec.dat, aes(x=year, y=log_recruitment)) + geom_point() + geom_line() +
+  facet_wrap(~species, scales="free") +
+  ylab("log(Recruitment)") + xlab("") + theme_bw()
 
 
 
