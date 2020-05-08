bf76459da5c1ed9b1e014069cdb94bad817257a8
KristaOke <kristaoke@gmail.com>
2020-05-07 18:18:54 -0800

    added some proof of concept gams on only pollock

--- scripts/trawl_biomass_GAM_explor.R
+++ scripts/trawl_biomass_GAM_explor.R
@@ -15,13 +15,32 @@ library(mgcv)
 sel.trawl.dat <- read.csv("data/select_trawl_dat.csv", row.names = 1)
 
 
-#going to need to get into a wider database I think
+#going to need to get into a wider database I think to get other sps in
+#and I need to only use early years!
+#don't smooth year just factor
+
 #try w just pollock first
-onlysmoothers <- gam(logCPUE ~ s(YEAR) + ts(LATITUDE, LONGITUDE), 
-            data=sel.trawl.dat[which(sel.trawl.dat$SCIENTIFIC=="Gadus chalcogrammus"),])
 
+#proof of concept gams=====
 
+yr_int <- gam(logCPUE ~ YEAR + s(LATITUDE, LONGITUDE, YEAR), 
+                      data=sel.trawl.dat[which(sel.trawl.dat$SCIENTIFIC=="Gadus chalcogrammus"),])
+summary(yr_int)
+plot(yr_int)
+vis.gam(yr_int, view=c("LATITUDE","LONGITUDE"))
+#look at predict.gam
 
+yr_botT_int <- gam(logCPUE ~ YEAR + BOT_TEMP + s(LATITUDE, LONGITUDE, YEAR), 
+              data=sel.trawl.dat[which(sel.trawl.dat$SCIENTIFIC=="Gadus chalcogrammus"),])
+summary(yr_botT_int)
+plot(yr_botT_int)
+vis.gam(yr_botT_int, view=c("LATITUDE","LONGITUDE"))
+
+yr_botTD_int <- gam(logCPUE ~ YEAR + BOT_TEMP + BOT_DEPTH + s(LATITUDE, LONGITUDE, YEAR), 
+                   data=sel.trawl.dat[which(sel.trawl.dat$SCIENTIFIC=="Gadus chalcogrammus"),])
+summary(yr_botTD_int)
+plot(yr_botTD_int)
+vis.gam(yr_botTD_int, view=c("LATITUDE","LONGITUDE"))
 
 
 
