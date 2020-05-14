1d7cac4db461b45540d904a4c240e4f4f41e5b6f
KristaOke <kristaoke@gmail.com>
2020-05-13 17:10:58 -0800

    more data wrangling

--- scripts/trawl_biomass_GAM_explor.R
+++ scripts/trawl_biomass_GAM_explor.R
@@ -32,32 +32,85 @@ early_dat <- early_dat[,-c(9,11)] #drop columns that repeat info
 #                                                      DATETIME, VESSEL, CRUISE, HAUL), 
 #                                         values_from=c(WTCPUE, NUMCPUE, logCPUE))
 
-early_wide_test <- early_dat %>% pivot_wider(names_from=SCIENTIFIC, 
+early_wide <- early_dat %>% pivot_wider(names_from=SCIENTIFIC, 
                                         values_from=c(WTCPUE, NUMCPUE, logCPUE))
+#eek this causes some bad column names!
+early_wide <- early_wide %>% rename(WTCPUE_Chionoecetes_bairdi = "WTCPUE_Chionoecetes bairdi",            
+  WTCPUE_Atheresthes_stomia = "WTCPUE_Atheresthes stomias",             
+  WTCPUE_Hippoglossus_stenolepis = "WTCPUE_Hippoglossus stenolepis",
+  WTCPUE_Limanda_aspera = "WTCPUE_Limanda aspera",         
+  WTCPUE_Lepidopsetta_sp= "WTCPUE_Lepidopsetta sp.",               
+  WTCPUE_Chionoecetes_opilio = "WTCPUE_Chionoecetes opilio",             
+  WTCPUE_Gadus_macrocephalus = "WTCPUE_Gadus macrocephalus",            
+  WTCPUE_Hippoglossoides_elassodon = "WTCPUE_Hippoglossoides elassodon",  
+  WTCPUE_Pleuronectes_quadrituberculatus = "WTCPUE_Pleuronectes quadrituberculatus", 
+  WTCPUE_Lepidopsetta_polyxystra = "WTCPUE_Lepidopsetta polyxystra", 
+  NUMCPUE_Gadus_chalcogrammus = "NUMCPUE_Gadus chalcogrammus",        
+  NUMCPUE_Chionoecetes_bairdi = "NUMCPUE_Chionoecetes bairdi",            
+  NUMCPUE_Atheresthes_stomias = "NUMCPUE_Atheresthes stomias",           
+  NUMCPUE_Hippoglossus_stenolepis = "NUMCPUE_Hippoglossus stenolepis",        
+  NUMCPUE_Limanda_aspera = "NUMCPUE_Limanda aspera",                
+  NUMCPUE_Lepidopsetta_sp = "NUMCPUE_Lepidopsetta sp.",               
+  NUMCPUE_Chionoecetes_opilio = "NUMCPUE_Chionoecetes opilio",           
+  NUMCPUE_Gadus_macrocephalus = "NUMCPUE_Gadus macrocephalus",            
+  NUMCPUE_Hippoglossoides_elassodon = "NUMCPUE_Hippoglossoides elassodon",     
+  NUMCPUE_Pleuronectes_quadrituberculatus = "NUMCPUE_Pleuronectes quadrituberculatus",
+  NUMCPUE_Lepidopsetta_polyxystra = "NUMCPUE_Lepidopsetta polyxystra",
+  logCPUE_Gadus_chalcogrammus = "logCPUE_Gadus chalcogrammus",        
+  logCPUE_Chionoecetes_bairdi = "logCPUE_Chionoecetes bairdi",           
+  logCPUE_Atheresthes_stomias = "logCPUE_Atheresthes stomias",            
+  logCPUE_Hippoglossus_stenolepis = "logCPUE_Hippoglossus stenolepis", 
+  logCPUE_Limanda_aspera = "logCPUE_Limanda aspera",        
+  logCPUE_Lepidopsetta_sp = "logCPUE_Lepidopsetta sp.",              
+  logCPUE_Chionoecetes_opilio = "logCPUE_Chionoecetes opilio",            
+  logCPUE_Gadus_macrocephalus = "logCPUE_Gadus macrocephalus",           
+  logCPUE_Hippoglossoides_elassodon = "logCPUE_Hippoglossoides elassodon",      
+  logCPUE_Pleuronectes_quadrituberculatus = "logCPUE_Pleuronectes quadrituberculatus" ,
+  logCPUE_Lepidopsetta_polyxystra = "logCPUE_Lepidopsetta polyxystra") 
 
 #try w just pollock first
 
 #proof of concept gams=====
 #confirm whether these should be te() or t2() or s()
 yr_int <- gam(logCPUE ~ YEAR_factor + s(LATITUDE, LONGITUDE, YEAR), 
-                      data=early_wide_test[which(early_wide_test$SCIENTIFIC=="Gadus chalcogrammus"),])
+                      data=sel.trawl.dat[which(sel.trawl.dat$SCIENTIFIC=="Gadus chalcogrammus"),])
 summary(yr_int)
 plot(yr_int)
 vis.gam(yr_int, view=c("LATITUDE","LONGITUDE"))
 #look at predict.gam
 
 yr_botT_int <- gam(logCPUE ~ YEAR_factor + BOT_TEMP + s(LATITUDE, LONGITUDE, YEAR), 
-              data=early_wide_test[which(early_wide_test$SCIENTIFIC=="Gadus chalcogrammus"),])
+              data=sel.trawl.dat[which(sel.trawl.dat$SCIENTIFIC=="Gadus chalcogrammus"),])
 summary(yr_botT_int)
 plot(yr_botT_int)
 vis.gam(yr_botT_int, view=c("LATITUDE","LONGITUDE"))
 
 yr_botTD_int <- gam(logCPUE ~ YEAR_factor + BOT_TEMP + BOT_DEPTH + s(LATITUDE, LONGITUDE, YEAR), 
-                   data=early_wide_test[which(early_wide_test$SCIENTIFIC=="Gadus chalcogrammus"),])
+                   data=sel.trawl.dat[which(sel.trawl.dat$SCIENTIFIC=="Gadus chalcogrammus"),])
 summary(yr_botTD_int)
 plot(yr_botTD_int)
 vis.gam(yr_botTD_int, view=c("LATITUDE","LONGITUDE"))
 
 
+#now with other sps===========
 
+spscovar1 <- gam(logCPUE_Gadus chalcogrammus ~ YEAR_factor + 
+"logCPUE_Chionoecetes bairdi" + "logCPUE_Atheresthes stomias" +           
+                "logCPUE_Hippoglossus stenolepis" + "logCPUE_Limanda aspera" +             
+                 "logCPUE_Lepidopsetta sp." + "logCPUE_Chionoecetes opilio" +            
+                 "logCPUE_Gadus macrocephalus" + "logCPUE_Hippoglossoides elassodon" +    
+                 "logCPUE_Pleuronectes quadrituberculatus" + "logCPUE_Lepidopsetta polyxystra" +   
+                   s(LATITUDE, LONGITUDE, YEAR), 
+              data=early_wide)
+summary(spscovar1)
+plot(spscovar1)
+vis.gam(spscovar1, view=c("LATITUDE","LONGITUDE"))
 
+spscovar1 <- gam("logCPUE_Gadus chalcogrammus" ~ YEAR_factor + 
+                  # "logCPUE_Chionoecetes bairdi" + "logCPUE_Atheresthes stomias" +           
+                   # "logCPUE_Hippoglossus stenolepis" + "logCPUE_Limanda aspera" +             
+                   # "logCPUE_Lepidopsetta sp." + "logCPUE_Chionoecetes opilio" +            
+                   # "logCPUE_Gadus macrocephalus" + "logCPUE_Hippoglossoides elassodon" +    
+                   # "logCPUE_Pleuronectes quadrituberculatus" + "logCPUE_Lepidopsetta polyxystra" +   
+                   s(LATITUDE, LONGITUDE, YEAR), 
+                 data=early_wide)
