72944d2aaf0850f6ab3fc895032ea78d698adfcb
KristaOke <kristaoke@gmail.com>
2020-08-17 16:17:45 -0800

    new section without sex

--- scripts/GAMs_binned_by_length.R
+++ scripts/GAMs_binned_by_length.R
@@ -1679,5 +1679,42 @@ visreg(tmod5E.1UK, "bottemp_anom", "period") #no data with low anom in late peri
 visreg(tmod5E.1UK, "mean_station_bottemp", "BOT_DEPTH")
 
 
+#Models without sex=======
+
+
+binmeta$STATION <- as.factor(binmeta$STATION)
+binmeta$VESSEL <- as.factor(binmeta$VESSEL)
+binmeta$CRUISE <- as.factor(binmeta$CRUISE)
+binmeta$HAUL <- as.factor(binmeta$HAUL)
+binmeta$bin <- as.factor(binmeta$bin)
+
+##exclusion criteria===============
+
+binmeta$log_sum_WGTCPUE_LEN <- log(binmeta$bin_sum_WGTCPUE_LEN)
+binmeta2$log_sum_WGTCPUE_LEN <- log(binmeta2$bin_sum_WGTCPUE_LEN)
+
+station_bin <- binmeta %>% group_by(STATION) %>%
+  summarize(n_yrs=n()) #not going to work, repeated years b/c diff bins
+
+joinstat <- left_join(binmeta, station_bin)
+
+bin_analysis_dat <- joinstat[which(joinstat$n_yrs>5),] #is this too liberal?
+table(joinstat$STATION, joinstat$bin)
+
+
+
+z11 <- ggplot(bin_analysis_dat[which(bin_analysis_dat$YEAR==2000),], aes(LONGITUDE, LATITUDE, 
+                                                                         colour=log_sum_WGTCPUE_LEN))
+z11 + geom_point() +   scale_colour_gradient2(low="blue", high="red", guide="colorbar")
+
+z11 <- ggplot(bin_analysis_dat, aes(LONGITUDE, LATITUDE, 
+                                    colour=log_sum_WGTCPUE_LEN))
+z11 + geom_point() +   scale_colour_gradient2(low="blue", high="red", guide="colorbar")
+
+bin1dat <- binmeta2[which(binmeta2$bin=="0-200"),]
+bin2dat <- binmeta2[which(binmeta2$bin=="200-300"),]
+bin3dat <- binmeta2[which(binmeta2$bin=="300-400"),]
+bin4dat <- binmeta2[which(binmeta2$bin=="400-500"),]
+bin5dat <- binmeta2[which(binmeta2$bin=="500+"),]
 
 
