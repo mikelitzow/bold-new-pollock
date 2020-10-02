584be08271774d26adc00667716692bb55955b0e
KristaOke <kristaoke@gmail.com>
2020-10-02 11:50:49 -0800

    setting up to loop

--- scripts/windows_breakpoint_loops.R
+++ scripts/windows_breakpoint_loops.R
@@ -11,7 +11,14 @@ wd <- getwd()
 loops_dat <- read.csv(file=paste(wd,"/data/processed_periods_analysis_data.csv", sep=""), row.names = 1)
 names(loops_dat)
 
+yrs <- unique(loops_dat$YEAR)
+n_yrs <- length(yrs)
+possible_breaks <- yrs[which(yrs>(1982+5) & yrs<(2019-5))]
 
+i<-1
+for(i in 1:length(possible_breaks)){
+  print(possible_breaks[i])
+}
 
 
 
