c2689217b88d6c3e6a7036ca6d82be2277f4107b
KristaOke <kristaoke@gmail.com>
2020-10-02 10:46:00 -0800

    new scrirpt to loop through potential windows and breakpoints

---
+++ scripts/windows_breakpoint_loops.R
@@ -0,0 +1,27 @@
+#====================================================================================================================
+#Looping through to test different windows and breakpoints
+#
+#by Krista, Oct-2020
+#====================================================================================================================
+#Notes:
+#====================================================================================================================
+
+#read in periods_analysis_data from trawl_biomass_GAM_explor but rename
+wd <- getwd()
+loops_dat <- read.csv(file=paste(wd,"/data/processed_periods_analysis_data.csv", sep=""), row.names = 1)
+names(loops_dat)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
