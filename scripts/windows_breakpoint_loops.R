6a39e4e2558cec2402d8ca6d5513106321991b20
KristaOke <kristaoke@gmail.com>
2020-10-04 11:16:38 -0800

    added a new loop that cuts data off so only 6 yrs post each break

--- scripts/windows_breakpoint_loops.R
+++ scripts/windows_breakpoint_loops.R
@@ -61,14 +61,59 @@ for(i in 1:length(possible_breaks)){
   
 }
 
+outputdf <- as.data.frame(cbind(breakpoint_vec, AIC_split_vec, AIC_drop_vec))
+
+outputdf$difference_AIC <- outputdf$AIC_split_vec - outputdf$AIC_drop_vec
+
+
+#next cut off data six yrs after breakpoint and try again
 
 
 
+breakpoint_six_vec <- vector(mode="numeric", length=length(possible_breaks))
+AIC_split_six_vec <- vector(mode="numeric", length=length(possible_breaks))
+AIC_drop_six_vec <- vector(mode="numeric", length=length(possible_breaks))
 
 
+i<-1
+for(i in 1:length(possible_breaks)){
+  #set up data with new breakpoint
+  print(possible_breaks[i])
+  temp_break <- possible_breaks[i]
+  temp_dat <- loops_dat[which(loops_dat$YEAR<(temp_break+5))]
+  temp_dat$split <- NA
+  temp_dat$split[which(temp_dat$YEAR>(temp_break-1))] <- "post"
+  temp_dat$split[which(temp_dat$YEAR<(temp_break))] <- "pre"
+  
+  temp_dat$split <- as.factor(temp_dat$split)
+  
+  #run model
+  temp_mod <- gamm(logCPUE_Gadus_chalcogrammus ~ bottemp_anom:split + ti(mean_station_bottemp, BOT_DEPTH) +
+                     s(YEAR_factor, bs="re"), 
+                   correlation = corExp(form=~ long_albers + lat_albers|YEAR_factor, nugget=TRUE),
+                   data=temp_dat)
+  
+  #save output
+  breakpoint_six_vec[i] <- temp_break      
+  AIC_split_six_vec[i] <- AIC(temp_mod[[1]])
+  saveRDS(temp_mod, file=paste(wd,"/data/breakpoint_six_model", temp_break, ".csv", sep=""))
+  
+  
+  #run drop model
+  temp_drop <- gamm(logCPUE_Gadus_chalcogrammus ~ bottemp_anom + ti(mean_station_bottemp, BOT_DEPTH) +
+                      s(YEAR_factor, bs="re"), 
+                    correlation = corExp(form=~ long_albers + lat_albers|YEAR_factor, nugget=TRUE),
+                    data=temp_dat)
+  
+  #save output
+  AIC_drop_six_vec[i] <- AIC(temp_drop[[1]])
+  saveRDS(temp_drop, file=paste(wd,"/data/breakpoint_six_drop_model", temp_break, ".csv", sep=""))
+  
+}
 
+outputdf_six <- as.data.frame(cbind(breakpoint_six_vec, AIC_split_six_vec, AIC_drop_six_vec))
 
-
+outputdf_six$difference_AIC <- outputdf_six$AIC_split_six_vec - outputdf_six$AIC_drop_six_vec
 
 
 
