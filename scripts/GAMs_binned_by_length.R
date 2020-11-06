6e83782c9cc97f2a2ba6715b9b921f6978c071d6
KristaOke <kristaoke@gmail.com>
2020-11-06 10:50:34 -0900

    updating for new output and removing old stuff

--- scripts/GAMs_binned_by_length.R
+++ scripts/GAMs_binned_by_length.R
@@ -4,85 +4,42 @@
 library(ggplot2)
 library(sjPlot)
 
-binmeta$log_sum_WGTCPUE_LEN <- log(binmeta$bin_sum_WGTCPUE_LEN)
-binmeta2$log_sum_WGTCPUE_LEN <- log(binmeta2$bin_sum_WGTCPUE_LEN)
+binmeta_clean$log_sum_WGTCPUE_LEN <- log(binmeta_clean$sum_wgtCPUE_len + 1)
 
 
-binmeta$STATION <- as.factor(binmeta$STATION)
-binmeta$VESSEL <- as.factor(binmeta$VESSEL)
-binmeta$CRUISE <- as.factor(binmeta$CRUISE)
-binmeta$HAUL <- as.factor(binmeta$HAUL)
-binmeta$bin <- as.factor(binmeta$bin)
+binmeta_clean$STATION <- as.factor(binmeta_clean$STATION)
+binmeta_clean$VESSEL <- as.factor(binmeta_clean$VESSEL)
+binmeta_clean$CRUISE <- as.factor(binmeta_clean$CRUISE)
+binmeta_clean$HAUL <- as.factor(binmeta_clean$HAUL)
+binmeta_clean$bin <- as.factor(binmeta_clean$bin)
 
 ##exclusion criteria===============
 
-station_bin <- binmeta %>% group_by(STATION) %>%
+#how to exclude stations w few obs? Maybe n in a single bin? yeah do once split out
+
+
+bin1dat <- binmeta_clean[which(binmeta_clean$bin=="0-200"),]
+bin2dat <- binmeta_clean[which(binmeta_clean$bin=="200-300"),]
+bin3dat <- binmeta_clean[which(binmeta_clean$bin=="300-400"),]
+bin4dat <- binmeta_clean[which(binmeta_clean$bin=="400-500"),]
+bin5dat <- binmeta_clean[which(binmeta_clean$bin=="500+"),]
+
+
+
+station_bin <- binmeta_clean %>% group_by(STATION) %>%
   summarize(n_yrs=n()) #not going to work, repeated years b/c diff bins
 
-joinstat <- left_join(binmeta, station_bin)
+joinstat <- left_join(binmeta_clean, station_bin)
 
 bin_analysis_dat <- joinstat[which(joinstat$n_yrs>5),] #is this too liberal?
 table(joinstat$STATION, joinstat$bin)
 
-z11 <- ggplot(bin_analysis_dat[which(bin_analysis_dat$YEAR==2000),], aes(LONGITUDE, LATITUDE, 
-                                           colour=log_sum_WGTCPUE_LEN))
-z11 + geom_point() +   scale_colour_gradient2(low="blue", high="red", guide="colorbar")
-
-z11 <- ggplot(bin_analysis_dat, aes(LONGITUDE, LATITUDE, 
-                                                                         colour=log_sum_WGTCPUE_LEN))
-z11 + geom_point() +   scale_colour_gradient2(low="blue", high="red", guide="colorbar")
-
-
-  
- 
-#Models without sex=======
-
-
-binmeta$STATION <- as.factor(binmeta$STATION)
-binmeta$VESSEL <- as.factor(binmeta$VESSEL)
-binmeta$CRUISE <- as.factor(binmeta$CRUISE)
-binmeta$HAUL <- as.factor(binmeta$HAUL)
-binmeta$bin <- as.factor(binmeta$bin)
-
-##exclusion criteria===============
-
-binmeta$log_sum_WGTCPUE_LEN <- log(binmeta$bin_sum_WGTCPUE_LEN)
-binmeta2$log_sum_WGTCPUE_LEN <- log(binmeta2$bin_sum_WGTCPUE_LEN)
-
-station_bin <- binmeta %>% group_by(STATION) %>%
-  summarize(n_yrs=n()) #not going to work, repeated years b/c diff bins
-
-joinstat <- left_join(binmeta, station_bin)
-
-bin_analysis_dat <- joinstat[which(joinstat$n_yrs>5),] #is this too liberal?
-table(joinstat$STATION, joinstat$bin)
-
-binmeta2$bin <- as.factor(binmeta2$bin)
-
-z11 <- ggplot(bin_analysis_dat[which(bin_analysis_dat$YEAR==2000),], aes(LONGITUDE, LATITUDE, 
-                                                                         colour=log_sum_WGTCPUE_LEN))
-z11 + geom_point() +   scale_colour_gradient2(low="blue", high="red", guide="colorbar")
-
-z11 <- ggplot(bin_analysis_dat, aes(LONGITUDE, LATITUDE, 
-                                    colour=log_sum_WGTCPUE_LEN))
-z11 + geom_point() +   scale_colour_gradient2(low="blue", high="red", guide="colorbar")
-
-bin1dat <- binmeta2[which(binmeta2$bin=="0-200"),]
-bin2dat <- binmeta2[which(binmeta2$bin=="200-300"),]
-bin3dat <- binmeta2[which(binmeta2$bin=="300-400"),]
-bin4dat <- binmeta2[which(binmeta2$bin=="400-500"),]
-bin5dat <- binmeta2[which(binmeta2$bin=="500+"),]
-
-
-ggplot(binmeta2, aes(bin, log_sum_WGTCPUE_LEN)) + geom_boxplot()
-
-ggplot(binmeta2, aes(bottemp_anom, log_sum_WGTCPUE_LEN, colour=bin)) + geom_point() + geom_smooth()
 
 
 
 
 #model selection======
 
-binmeta2$bin <- as.factor(binmeta2$bin)
+binmeta_clean$bin <- as.factor(binmeta_clean$bin)
 
 
