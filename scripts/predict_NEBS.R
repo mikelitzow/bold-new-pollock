43bc8b6b38f92483ea20613a0cd904c8ba1312f5
KristaOke <kristaoke@gmail.com>
2020-11-16 16:20:05 -0900

    more data processing to get ready to predict

--- scripts/predict_NEBS.R
+++ scripts/predict_NEBS.R
@@ -11,6 +11,18 @@
 pmod <- read_rds("~/Dropbox/Work folder/Pollock Analyses/bold-new-pollock/scripts/lin-int_allages_model.RDS")
 summary(pmod)
 
+#set up data to 1) drop high mean stations temps and 2) set high mean station temps to highest observed in SEBS
+# all_analysis_dat from NEBS_depth_temp_plot.R
+#needs lat/long albers and period
+
+#what is highest mean station bottemp in early?
+maxmean_bottemp_early <- max(all_analysis_dat$mean_station_bottemp[which(all_analysis_dat$YEAR<2014)])
+#11.7
+
+pdat_limited <- all_analysis_dat[which(all_analysis_dat$mean_station_bottemp<maxmean_bottemp_early),]
+
+#just cols used for prediction
+pdat_sel <- pdat_limited[,c(21:23)]
 
 
 
