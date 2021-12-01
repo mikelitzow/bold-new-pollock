685afb22de8d84051f4db41d1effbdaf116945a4
KristaOke <kristaoke@gmail.com>
2021-11-30 15:12:05 -0900

    cleaned up plot a little

--- scripts/north-south ERSST time series comparison.R
+++ scripts/north-south ERSST time series comparison.R
@@ -12,3 +12,21 @@ ggplot(dat, aes(year, value)) +
   geom_line() +
   geom_point() +
   facet_wrap(~name, ncol=2)
+
+dat$season <- NA
+
+dat$season[which(dat$name=="south.sst.ndjfm" | dat$name=="north.sst.ndjfm")] <- "winter"
+dat$season[which(dat$name=="south.sst.amj" | dat$name=="north.sst.amj")] <- "spring"
+
+dat$region <- NA
+
+dat$region[which(dat$name=="south.sst.ndjfm" | dat$name=="south.sst.amj")] <- "south"
+dat$region[which(dat$name=="north.sst.ndjfm" | dat$name=="north.sst.amj")] <- "north"
+
+
+ggplot(dat, aes(year, value, colour=region)) +
+  geom_line() +
+  geom_point() +
+  facet_wrap(~season) + ylab("SST") + xlab("Year") + theme_bw() +
+  theme(legend.position = c(0.75, 0.9), legend.direction = "horizontal") +
+  scale_colour_manual(values=c("black", "dark grey"))
