3d571380b199d62e637054e8a023515d8c11240b
KristaOke <kristaoke@gmail.com>
2022-06-27 14:22:27 -0800

    updating with new data

--- scripts/NEBS_depth_temp_plot.R
+++ scripts/NEBS_depth_temp_plot.R
@@ -5,7 +5,11 @@
 #use pol.trawl.dat instead from data_explor_survey.R
 
 wd <- getwd()
-sel.trawl.dat <- read.csv(paste(wd,"/data/select_trawl_dat.csv",sep=""), row.names = 1)
+# sel.trawl.dat <- read.csv(paste(wd,"/data/select_trawl_dat.csv",sep=""), row.names = 1)
+
+#updating to use new nebs data
+newdat <- read.csv(file=paste(wd,"/data/survey data/cleaned_nebs_and_2021_bot_trawl_data.csv", sep=""))
+
 
 
 #to do this I am going to need to:
@@ -18,26 +22,27 @@ library("rnaturalearth")
 library("rnaturalearthdata")
 library( "ggspatial" )
 library("sf")
+library(mapproj)
 
-
-
-pol.trawl.dat$YEAR_factor <- as.factor(pol.trawl.dat$YEAR)
-pol.trawl.dat$BOT_TEMP[which(pol.trawl.dat$BOT_TEMP=="-9999")]<-NA
-pol.trawl.dat$SURF_TEMP[which(pol.trawl.dat$SURF_TEMP=="-9999")]<-NA
-pol.trawl.dat$WTCPUE[which(pol.trawl.dat$WTCPUE=="-9999")]<-NA
-pol.trawl.dat$NUMCPUE[which(pol.trawl.dat$NUMCPUE=="-9999")]<-NA
-
-pol.trawl.dat$LATITUDE  <- as.numeric(pol.trawl.dat$LATITUDE)
-pol.trawl.dat$LONGITUDE <- as.numeric(pol.trawl.dat$LONGITUDE)
-pol.trawl.dat$YEAR <- as.numeric(pol.trawl.dat$YEAR )
-pol.trawl.dat$WTCPUE <- as.numeric(pol.trawl.dat$WTCPUE)
-pol.trawl.dat$NUMCPUE <- as.numeric(pol.trawl.dat$NUMCPUE)
-pol.trawl.dat$BOT_DEPTH <- as.numeric(pol.trawl.dat$BOT_DEPTH )
-pol.trawl.dat$BOT_TEMP <- as.numeric(pol.trawl.dat$BOT_TEMP)
-pol.trawl.dat$SURF_TEMP <- as.numeric(pol.trawl.dat$SURF_TEMP)
-
-
-pol.trawl.dat$logCPUE <- log(pol.trawl.dat$WTCPUE + 1)
+# 
+# 
+# pol.trawl.dat$YEAR_factor <- as.factor(pol.trawl.dat$YEAR)
+# pol.trawl.dat$BOT_TEMP[which(pol.trawl.dat$BOT_TEMP=="-9999")]<-NA
+# pol.trawl.dat$SURF_TEMP[which(pol.trawl.dat$SURF_TEMP=="-9999")]<-NA
+# pol.trawl.dat$WTCPUE[which(pol.trawl.dat$WTCPUE=="-9999")]<-NA
+# pol.trawl.dat$NUMCPUE[which(pol.trawl.dat$NUMCPUE=="-9999")]<-NA
+# 
+# pol.trawl.dat$LATITUDE  <- as.numeric(pol.trawl.dat$LATITUDE)
+# pol.trawl.dat$LONGITUDE <- as.numeric(pol.trawl.dat$LONGITUDE)
+# pol.trawl.dat$YEAR <- as.numeric(pol.trawl.dat$YEAR )
+# pol.trawl.dat$WTCPUE <- as.numeric(pol.trawl.dat$WTCPUE)
+# pol.trawl.dat$NUMCPUE <- as.numeric(pol.trawl.dat$NUMCPUE)
+# pol.trawl.dat$BOT_DEPTH <- as.numeric(pol.trawl.dat$BOT_DEPTH )
+# pol.trawl.dat$BOT_TEMP <- as.numeric(pol.trawl.dat$BOT_TEMP)
+# pol.trawl.dat$SURF_TEMP <- as.numeric(pol.trawl.dat$SURF_TEMP)
+# 
+# 
+# pol.trawl.dat$logCPUE <- log(pol.trawl.dat$WTCPUE + 1)
 
 
 world <- ne_countries(scale = "medium", returnclass = "sf")
@@ -45,12 +50,12 @@ ggplot(data = world) +
   geom_sf() +
   coord_sf(xlim = c(-180, -155), ylim = c(53, 65), expand = TRUE) +
   annotation_scale(location = "bl", width_hint = 0.5) +
-  geom_point(aes(LONGITUDE, LATITUDE, colour=logCPUE), data=pol.trawl.dat) +   
+  geom_point(aes(LONGITUDE, LATITUDE, colour=logCPUE), data=newdat) +   
   scale_colour_gradient2(low="blue", high="red", guide="colorbar")  
 
-x <- mapproject(pol.trawl.dat$LONGITUDE, pol.trawl.dat$LATITUDE, "albers", param=c(55.9, 60.8))
-pol.trawl.dat$long_albers <- x$x
-pol.trawl.dat$lat_albers <- x$y
+x <- mapproject(newdat$LONGITUDE, newdat$LATITUDE, "albers", param=c(55.9, 60.8))
+newdat$long_albers <- x$x
+newdat$lat_albers <- x$y
 
 
 
@@ -60,12 +65,12 @@ pol.trawl.dat$lat_albers <- x$y
 #calculate anomalies and means=====================================================================
 
 #annual 'global' (w/in dataset) mean
-poltempmeans <- pol.trawl.dat %>% group_by(STATION) %>% summarize(mean_station_bottemp=mean(BOT_TEMP, na.rm=TRUE))
-poltempmeans <- left_join(pol.trawl.dat, poltempmeans )
-
-poltempmeans$bottemp_anom <- poltempmeans$BOT_TEMP - poltempmeans$mean_station_bottemp
-
-all_analysis_dat <- poltempmeans
+# poltempmeans <- pol.trawl.dat %>% group_by(STATION) %>% summarize(mean_station_bottemp=mean(BOT_TEMP, na.rm=TRUE))
+# poltempmeans <- left_join(pol.trawl.dat, poltempmeans )
+# 
+# poltempmeans$bottemp_anom <- poltempmeans$BOT_TEMP - poltempmeans$mean_station_bottemp
+# 
+# all_analysis_dat <- poltempmeans
 
 
 
@@ -111,10 +116,10 @@ ggplot(data = world) +
 
 #sort into NEBS and SEBS
 
-all_analysis_dat$region <- "SEBS"
-all_analysis_dat$region[which(all_analysis_dat$STRATUM==81 | 
-                                all_analysis_dat$STRATUM==70 |
-                                all_analysis_dat$STRATUM==71)] <- "NEBS"
+newdat$region <- "SEBS"
+newdat$region[which(newdat$STRATUM==81 | 
+                                newdat$STRATUM==70 |
+                                newdat$STRATUM==71)] <- "NEBS"
 
 ggplot(data = world) +
   geom_sf() +
@@ -123,27 +128,27 @@ ggplot(data = world) +
   # geom_point(aes(LONGITUDE, LATITUDE, colour=mean_station_bottemp), data=all_analysis_dat) +   
   # scale_colour_gradient2(low="blue", high="red", guide="colorbar") + 
   geom_point(aes(LONGITUDE, LATITUDE, 
-                col=region), data=all_analysis_dat) 
+                col=region), data=newdat) 
 
 #add shelves 
 #based on table 1 in Laurth et al 2019 NOAA Technical Memorandum NMFS-AFSC-396
 
-all_analysis_dat$shelf <- NA
-all_analysis_dat$shelf[which(all_analysis_dat$STRATUM==81 | 
-                                all_analysis_dat$STRATUM==70 |
-                                all_analysis_dat$STRATUM==71)] <- "NEBS"
-all_analysis_dat$shelf[which(all_analysis_dat$STRATUM==10 | 
-                               all_analysis_dat$STRATUM==20)] <- "EBS_inner"
-all_analysis_dat$shelf[which(all_analysis_dat$STRATUM==31 | 
-                               all_analysis_dat$STRATUM==32 | 
-                               all_analysis_dat$STRATUM==41 | 
-                               all_analysis_dat$STRATUM==42 | 
-                               all_analysis_dat$STRATUM==43 | 
-                               all_analysis_dat$STRATUM==82)] <- "EBS_middle"
-all_analysis_dat$shelf[which(all_analysis_dat$STRATUM==50 | 
-                               all_analysis_dat$STRATUM==61 | 
-                               all_analysis_dat$STRATUM==62 | 
-                               all_analysis_dat$STRATUM==90)] <- "EBS_outer"
+newdat$shelf <- NA
+newdat$shelf[which(newdat$STRATUM==81 | 
+                     newdat$STRATUM==70 |
+                     newdat$STRATUM==71)] <- "NEBS"
+newdat$shelf[which(newdat$STRATUM==10 | 
+                     newdat$STRATUM==20)] <- "EBS_inner"
+newdat$shelf[which(newdat$STRATUM==31 | 
+                     newdat$STRATUM==32 | 
+                     newdat$STRATUM==41 | 
+                     newdat$STRATUM==42 | 
+                     newdat$STRATUM==43 | 
+                     newdat$STRATUM==82)] <- "EBS_middle"
+newdat$shelf[which(newdat$STRATUM==50 | 
+                     newdat$STRATUM==61 | 
+                     newdat$STRATUM==62 | 
+                     newdat$STRATUM==90)] <- "EBS_outer"
 
 load(paste(wd,"/data/Bering_Chukchi_contours.RData",sep=""))
 
